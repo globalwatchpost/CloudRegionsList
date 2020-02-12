795afb561e45183c33b6e388292a99856cc7e10f
Terry Ott <Terry.Ott@globalwatchpost.com>
2020-02-12 02:35:35 +0000

    Azure nicely populated

--- site_generation/cloudprovider_azure.py
+++ site_generation/cloudprovider_azure.py
@@ -58,6 +58,11 @@ class CloudProviderAzure(cloudprovider.CloudProvider):
                     "iso_3166-1"    : [ "???" ],
                     "iso_3166-2"    : [ "???" ]
                 }
-            
+
+        # doublecheck against our JSON list, add any that our account can't see
+        for providerRegionId in self._regionGeoInfo:
+            if providerRegionId not in self._regions:
+                self._regions[ providerRegionId ] = self._regionGeoInfo[ providerRegionId ]
+                logging.info( "Added in region {0} from JSON, not found in CLI results".format(providerRegionId) )
 
         return self._regions
---
+++ site_generation/content/pages/aws-azure_country_asc.md
@@ -0,0 +1,6 @@
+Title: AWS+Azure Country Ascending
+Date: 2020-01-26 18:35:00
+Modified: 2020-01-26 18:35:00
+url: AWS-Azure/display_countries/asc.html
+Save_as: AWS-Azure/display_countries/asc.html
+Template: cloud-list-template
